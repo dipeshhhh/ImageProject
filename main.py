6c878349888044f417b773873fdfaa00e49e969a
Dipesh <dipesh.productivity@gmail.com>
2022-12-09 10:40:52 +0530

    UPDATE: Added crop functinality

--- main.py
+++ main.py
@@ -9,24 +9,31 @@ from tkinter import *
 from customtkinter import *
 from customtkinter import filedialog
 from PIL import Image, ImageOps, ImageTk, ImageEnhance
-
+#ignore::CTkLabelWarning
 
 def re_frame(image_name, frame_name):
     # Empties frame then adds PhotoImage to grid(row=0,column=0)
     # image_name: ImageTk.PhotoImage(), frame_name: CTkFrame()
+    show_image_name = ImageTk.PhotoImage(image_name)
 
     for widget in frame_name.winfo_children():
         widget.destroy()
 
-    label_img_preview = CTkLabel(frame_name, image=image_name, text="")
+    label_img_preview = CTkLabel(frame_name, image=show_image_name, text="")
     label_img_preview.grid(row=0,column=0)  
 
 def open_file():
     # Opens image and resizes it to fit the window
     global img, img_preview, previewIMG_frame
 
+    image_extentions = (
+        ("All Files","*.*"),
+        ("PNG","*.png"),
+        ("JPG","*.jpg"),
+        ("JPEG","*.jpeg"))
+
     image_path = filedialog.askopenfilename(initialdir="./", title="Open your image",
-                filetypes=(("All types", "*.*"), ("png", "*.png"), ("jpg", "*.jpg")))
+                filetypes=image_extentions)
     
     # Get the current tkinter window width and height
     window_width = root.winfo_width()
@@ -35,9 +42,9 @@ def open_file():
 
     img = Image.open(image_path)
     img_preview = ImageOps.contain(img,(width_inputIMG,window_height))
-    show_img_preview = ImageTk.PhotoImage(img_preview)
+    # show_img_preview = ImageTk.PhotoImage(img_preview)
 
-    re_frame(show_img_preview, previewIMG_frame)
+    re_frame(img_preview, previewIMG_frame)
 
 def undo_change():
     # Reverts back to previous change
@@ -71,9 +78,9 @@ def rotate_image(direction):
         img = img.rotate(-90, expand=True)
         img_preview = img_preview.rotate(-90, expand=True)
 
-    show_img_preview = ImageTk.PhotoImage(img_preview)
+    # show_img_preview = ImageTk.PhotoImage(img_preview)
 
-    re_frame(show_img_preview, previewIMG_frame) 
+    re_frame(img_preview, previewIMG_frame) 
 
 def flip_image(orientation):
     # Flips both <img> and <img_preview> in given orientation = vertical/horizontal
@@ -86,9 +93,9 @@ def flip_image(orientation):
         img = img.transpose(Image.TRANSPOSE.FLIP_LEFT_RIGHT)
         img_preview = img_preview.transpose(Image.TRANSPOSE.FLIP_LEFT_RIGHT)
 
-    show_image_preview = ImageTk.PhotoImage(img_preview)
+    # show_image_preview = ImageTk.PhotoImage(img_preview)
 
-    re_frame(show_image_preview, previewIMG_frame)
+    re_frame(img_preview, previewIMG_frame)
 
 def black_and_white():
     # Toggles greyscale in the image
@@ -109,18 +116,117 @@ def black_and_white():
 
         isGrayscale = True
     
-    show_image_preview = ImageTk.PhotoImage(img_preview)
-    re_frame(show_image_preview, previewIMG_frame)
+    # show_image_preview = ImageTk.PhotoImage(img_preview)
+    re_frame(img_preview, previewIMG_frame)
 
 def add_text(img, img_preview):
     # Adds text with white background to bottom of the image
 
     pass
 
-def image_crop(img, img_preview):
-    # Crops image while keeping the original height and width ratio
+def image_crop():
+    # Crops image
+    crop_function = CTkToplevel()
+    crop_function.title("Crop Image")
+    global img, img_preview, cropped_image
+    
+    img_width, img_height = img.size
+    img_preview_width, img_preview_height = img_preview.size
+    show_img_preview = ImageTk.PhotoImage(img_preview)
 
-    pass
+    img_to_preview_ratio = img_width/img_preview_width    
+
+    def crop(left, top, right, bottom):
+        # Crops and Displays in real-time
+        global cropped_image, Left, Right, Top, Bottom
+
+        crop_image = img_preview.copy()
+        cropped_image = crop_image.crop((left, top, img_preview_width-right, img_preview_height-bottom))
+        show_cropped_image = ImageTk.PhotoImage(cropped_image)
+        
+        label_show_preview_img.configure(image=show_cropped_image)
+        
+        # Updating Values for original img
+        Left = left*img_to_preview_ratio
+        Top = top*img_to_preview_ratio
+        Right = right*img_to_preview_ratio
+        Bottom = bottom*img_to_preview_ratio
+
+    def save():
+        # Updates the original image with cropped one
+
+        global img_preview, img, Left, Right, Top, Bottom
+
+        window_width = root.winfo_width()
+        window_height = root.winfo_height()
+        width_inputIMG = int((window_width/2)-46)
+
+        img_preview = ImageOps.contain(cropped_image,(width_inputIMG,window_height))
+        img = img.crop((Left,Top,img_width-Right,img_height-Bottom))
+        re_frame(img_preview, previewIMG_frame)
+        crop_function.destroy()
+
+    # Sliders in Control Frame
+    control_frame = CTkFrame(crop_function)
+
+    label_left_text = CTkLabel(control_frame,text="Left")
+    label_left_text.grid(row=0,column=0, padx=2, pady=2)
+    left_slider = CTkSlider(master=control_frame,from_=0, to=img_preview_width, 
+                            orientation=HORIZONTAL,
+                            command=lambda X: crop(left_slider.get(),
+                                                    top_slider.get(),
+                                                    right_slider.get(),
+                                                    bottom_slider.get()))
+    left_slider.set(0)
+    left_slider.grid(row=0,column=1, padx=2, pady=2)
+
+    label_right_text = CTkLabel(control_frame,text="Right")
+    label_right_text.grid(row=1,column=0, padx=2, pady=2)
+    right_slider = CTkSlider(master=control_frame,from_=0, to=img_preview_width, 
+                            orientation=HORIZONTAL,
+                            command=lambda X: crop(left_slider.get(),
+                                                    top_slider.get(),
+                                                    right_slider.get(),
+                                                    bottom_slider.get()))
+    right_slider.set(0)
+    right_slider.grid(row=1,column=1, padx=2, pady=2)
+
+    label_topTXT = CTkLabel(control_frame,text="Top")
+    label_topTXT.grid(row=2,column=0, padx=2, pady=2)
+    top_slider = CTkSlider(master=control_frame,from_=0, to=img_preview_height, 
+                            orientation=HORIZONTAL,
+                            command=lambda X: crop(left_slider.get(),
+                                                    top_slider.get(),
+                                                    right_slider.get(),
+                                                    bottom_slider.get()))
+    top_slider.set(0)
+    top_slider.grid(row=2,column=1, padx=2, pady=2)
+
+    label_bottomTXT = CTkLabel(control_frame,text="Bottom")
+    label_bottomTXT.grid(row=3,column=0, padx=2, pady=2)
+    bottom_slider = CTkSlider(master=control_frame,from_=0, to=img_preview_height, 
+                            orientation=HORIZONTAL,
+                            command=lambda X: crop(left_slider.get(),
+                                                    top_slider.get(),
+                                                    right_slider.get(),
+                                                    bottom_slider.get()))
+    bottom_slider.set(0)
+    bottom_slider.grid(row=3,column=1, padx=2, pady=2)
+
+    cancel_button = CTkButton(control_frame, text="Cancel", command=crop_function.destroy)
+    cancel_button.grid(row=4,column=0,pady=20)
+
+    ok_button = CTkButton(control_frame,text="OK",command=save)
+    ok_button.grid(row=4,column=1)
+
+    control_frame.grid(row=0,column=2,padx=10,sticky=N+S)
+
+    # Creating image frame -> grid_propagate(0)
+    cropImage_Frame = CTkFrame(crop_function, width=img_preview_width+2, height=img_preview_height+2)
+    cropImage_Frame.grid_propagate(0)
+    label_show_preview_img = CTkLabel(master=cropImage_Frame,image = show_img_preview, text="")
+    label_show_preview_img.place(relx=0.5, rely=0.5, anchor=CENTER)
+    cropImage_Frame.grid(row=0,column=1, sticky=N+S)
 
 def save_as(img_output):
     # Saves the given image
@@ -188,9 +294,9 @@ def output_image(sheet_size):
     height_result_image = int(window_height-29)    
     
     result_image_preview = ImageOps.contain(result_image,(width_result_image,height_result_image))
-    show_result_image_preview = ImageTk.PhotoImage(result_image_preview)
+    # show_result_image_preview = ImageTk.PhotoImage(result_image_preview)
 
-    re_frame(show_result_image_preview, outputIMG_frame)
+    re_frame(result_image_preview, outputIMG_frame)
 
 ### ----Initializing Variables---- ###
 
@@ -203,6 +309,9 @@ result_image_preview=""
 isGrayscale = False
 grayscale_backup = ""
 grayscale_preview_backup = ""
+
+cropped_image = ""
+Left = Top = Right = Bottom = 0
 
 # 2D list to track changes made throughout the execution
 history_tracker=[] # [["<Previous image name>", "<Change happened to it>"], [...], ...]
@@ -246,7 +355,7 @@ button_rotate_clockwise = CTkButton(button_frame, text="", image=icon_rotate_clo
 button_flip_vertical = CTkButton(button_frame, text="", image=icon_flip_vertical, command = lambda: flip_image("VERTICAL"), width=40,height=80)
 button_flip_horizontal = CTkButton(button_frame, text="", image=icon_flip_horizontal, command = lambda: flip_image("HORIZONTAL"), width=40,height=80)
 button_grayscale = CTkButton(button_frame, text="", image=icon_grayscale, command = black_and_white, width=80, height=80)
-button_crop = CTkButton(button_frame, text="", image=icon_crop, command = lambda: image_crop("<X>","<X>"), width=80, height=80)
+button_crop = CTkButton(button_frame, text="", image=icon_crop, command = image_crop, width=80, height=80)
 button_add_text = CTkButton(button_frame, text="", image=icon_add_text, command = lambda: add_text("<X>","<X>"), width=80, height=80)
 
 button_rotate_anticlockwise.grid(row=0,column=0,padx=2,pady=2)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,1 @@
-
-    # for widget in outputIMG_frame.winfo_children():
-    #     widget.destroy()
-
-    
-    # label_result_image = CTkLabel(outputIMG_frame, image = show_result_image_preview, text="")
-    # label_result_image.grid(row=0,column=0)
+re_frame
